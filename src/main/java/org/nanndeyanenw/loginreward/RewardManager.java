6c485277b46c417dc02793eb74781830f1e5fd9c
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-15 10:00:14 +0900

    database廃止

--- src/main/java/org/nanndeyanenw/loginreward/RewardManager.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardManager.java
@@ -4,9 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,45 +18,7 @@ public class RewardManager {
     }
 
     public void loadData() {
-        try {
-            Connection connection = plugin.getDatabase().getConnection();
-            PreparedStatement statement = connection.prepareStatement("SELECT player, days FROM loginRewards");
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                Player player = plugin.getServer().getPlayer(resultSet.getString("player"));
-                int days = resultSet.getInt("days");
-                if (player != null) {
-                    consecutiveLogins.put(player, days);
-                }
-            }
-
-            statement.close();
-            resultSet.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void saveData() {
-        try {
-            Connection connection = plugin.getDatabase().getConnection();
-            PreparedStatement clearStatement = connection.prepareStatement("DELETE FROM loginRewards");
-            clearStatement.executeUpdate();
-
-            for (Map.Entry<Player, Integer> entry : consecutiveLogins.entrySet()) {
-                PreparedStatement statement = connection.prepareStatement("INSERT INTO loginRewards(player, days) VALUES(?, ?)");
-                statement.setString(1, entry.getKey().getUniqueId().toString());
-                statement.setInt(2, entry.getValue());
-                statement.executeUpdate();
-                statement.close();
-            }
-
-            clearStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        
 
     public Reward getReward(Player player) {
         int days = consecutiveLogins.getOrDefault(player, 0);
