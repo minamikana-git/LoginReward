81cae496f579b486ac606f71c8481df048e7436d
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-15 09:58:45 +0900

    database廃止

---
+++ src/main/java/org/nanndeyanenw/loginreward/LoginReward.java
@@ -0,0 +1,54 @@
+package org.nanndeyanenw.loginreward;
+
+import org.nanndeyanenw.loginreward.RewardGUI;
+import org.nanndeyanenw.loginreward.RewardManager;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class LoginReward extends JavaPlugin {
+
+       private RewardManager rewardManager;
+
+    private Economy economy;
+    private File dataFile;
+
+    @Override
+    public void onEnable() {
+            if (!setupEconomy()) { // 起動時のVault関係があるかどうか
+                getLogger().severe("エラー：Vaultプラグインが見つかりませんでした。プラグインを無効化します。");
+
+                if (getServer().getPluginManager().getPlugin("Vault") == null) {
+                    getLogger().severe("エラー：Vaultプラグインが見つかりません。");
+                } else {
+                    getLogger().severe("エラー：Economyサービスプロバイダが見つかりません。");
+                }
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+         }
+        this.rewardManager = new RewardManager(this);
+        getCommand("loginreward").setExecutor(RewardCommandExecutor(this));
+
+    }
+
+    @Override
+    public void onDisable() {
+        rewardManager.saveData();
+    }
+
+    private boolean setupEconomy() {
+        if (!setupEconomy()) { // 起動時のVault関係があるかどうか
+            getLogger().severe("エラー：Vaultプラグインが見つかりませんでした。プラグインを無効化します。");
+
+            if (getServer().getPluginManager().getPlugin("Vault") == null) {
+                getLogger().severe("エラー：Vaultプラグインが見つかりません。");
+            } else {
+                getLogger().severe("エラー：Economyサービスプロバイダが見つかりません。");
+            }
+            getServer().getPluginManager().disablePlugin(this);
+            return false;
+        }
+        return true;
+    }
+}
