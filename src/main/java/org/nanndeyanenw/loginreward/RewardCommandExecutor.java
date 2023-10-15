05af1acb5b4b02e877064a06a96b0b95deab3a8b
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-15 11:46:38 +0900

    fix code

--- src/main/java/org/nanndeyanenw/loginreward/RewardCommandExecutor.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardCommandExecutor.java
@@ -3,8 +3,7 @@ package org.nanndeyanenw.loginreward;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
-
+import org.bukkit.entity.Player;
 public class RewardCommandExecutor implements CommandExecutor {
 
     private final LoginReward loginReward;
@@ -15,8 +14,17 @@ public class RewardCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-        // コマンド処理の実装
-        return true; // または必要に応じて適切な戻り値を設定
+        if (command.getName().equalsIgnoreCase("loginreward")) {
+            if (!(commandSender instanceof Player)) {
+                commandSender.sendMessage("§cこのコマンドはプレイヤーからのみ実行できます。");
+                return true;
+            }
+            // プレイヤーとしての処理を続ける
+            Player player = (Player) commandSender;
+            // ここでプレイヤーに対する処理を実行
+            return true; // または必要に応じて適切な戻り値を設定
+        }
+        return true;
     }
 }
 
