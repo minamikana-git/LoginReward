e4d60911b6012bd6bca25fd508149b483015e617
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-15 15:16:28 +0900

    fix code

--- src/main/java/org/nanndeyanenw/loginreward/Reward.java
+++ src/main/java/org/nanndeyanenw/loginreward/Reward.java
@@ -43,17 +43,17 @@ public class Reward {
             case 1:
                 return new Reward(50, "ログインボーナス！: 50NANDE!");
             case 2:
-                return new Reward(100, "2 連続ログインボーナス！: 100NANDE!");
+                return new Reward(100, "2連続ログインボーナス！: 100NANDE!");
             case 3:
-                return new Reward(200, "3 連続ログインボーナス！: 200NANDE!");
+                return new Reward(200, "3連続ログインボーナス！: 200NANDE!");
             case 4:
-                return new Reward(400, "4 連続ログインボーナス！: 400NANDE!");
+                return new Reward(400, "4連続ログインボーナス！: 400NANDE!");
             case 5:
-                return new Reward(600, "5 連続ログインボーナス！: 600NANDE!");
+                return new Reward(600, "5連続ログインボーナス！: 600NANDE!");
             case 6:
-                return new Reward(800, "6 連続ログインボーナス！: 800NANDE!");
+                return new Reward(800, "6連続ログインボーナス！: 800NANDE!");
             case 7:
-                return new Reward(1000, "7 連続ログインボーナス！: 1000NANDE!");
+                return new Reward(1000, "7連続ログインボーナス！: 1000NANDE!");
             default:
                 return null;
         }
