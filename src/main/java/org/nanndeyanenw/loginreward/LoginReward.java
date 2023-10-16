05209efe4037ffd610e480cb02485bd3f2780b06
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-16 14:15:22 +0900

    書き直し

--- src/main/java/org/nanndeyanenw/loginreward/LoginReward.java
+++ src/main/java/org/nanndeyanenw/loginreward/LoginReward.java
@@ -11,11 +11,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class LoginReward extends JavaPlugin {
+
+    private Map<UUID, Double> playerMoney = new HashMap<>();
 
     private RewardManager rewardManager;
 
