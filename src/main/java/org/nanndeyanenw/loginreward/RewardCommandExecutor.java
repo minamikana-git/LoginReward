f11f5193018b186393d725660c444c614e7bd135
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-17 13:51:12 +0900

    少しずつ

--- src/main/java/org/nanndeyanenw/loginreward/RewardCommandExecutor.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardCommandExecutor.java
@@ -6,6 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class RewardCommandExecutor implements CommandExecutor {
 
     private final RewardManager rewardManager;
@@ -20,6 +23,24 @@ public class RewardCommandExecutor implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+
+            // 新しいコマンド "debugdate" を追加
+            if (cmd.getName().equalsIgnoreCase("debugdate")) {
+                if (!player.hasPermission("loginreward.debugdate")){
+                    player.sendMessage("§cあなたにはこのコマンドを実行する権限がありません。");
+                    return true;
+                }
+
+                player.sendMessage("§a時刻をアップデートしました。");
+
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String today = sdf.format(new Date());
+                String lastReceived = plugin.getPlayerDataConfig().getString(player.getUniqueId().toString() + ".lastReceived", "Not Found");
+
+                player.sendMessage("今日の日付: " + today);
+                player.sendMessage("最後に受け取った日付: " + lastReceived);
+                return true;
+            }
             if (!rewardManager.hasClaimedReward(player)) {
                 // GUIを表示
                 new RewardGUI(plugin).open(player);
