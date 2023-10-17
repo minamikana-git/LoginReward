dd75a5904b246bda8c23b855a613dbedac7f9ff8
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-17 14:54:56 +0900

    謎のエラー解消

--- src/main/java/org/nanndeyanenw/loginreward/RewardGUI.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardGUI.java
@@ -13,6 +13,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -66,84 +68,93 @@ public class RewardGUI implements Listener {
         String lastReceived = playerData.getString(player.getUniqueId().toString() + ".lastReceived", "");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String today = sdf.format(new Date());
-        return today.equals(lastReceived);
-    }
 
-    private void giveReward(Player player) {
-        int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
+        Bukkit.getLogger().info("Today: " + today); //デバッグ用コード
+        Bukkit.getLogger().info("Last received: " + lastReceived); //デバッグ用コード
 
-        double rewardAmount;
-        switch (daysLoggedIn) {
-            case 1:
-                rewardAmount = 50;
-                break;
-            case 2:
-                rewardAmount = 100;
-                break;
-            case 3:
-                rewardAmount = 200;
-                break;
-            case 4:
-                rewardAmount = 400;
-                break;
-            case 5:
-                rewardAmount = 600;
-                break;
-            case 6:
-                rewardAmount = 800;
-                break;
-            case 7:
-                rewardAmount = 1000;
-                break;
-            default:
-                rewardAmount = 50;
-                break;
+        if (!today.equals(lastReceived)) {
+            playerData.set(player.getUniqueId().toString() + ".lastReceived", today);
+            plugin.savePlayerDataConfig(); // ここでplayerDataを実際のファイルに保存
+            return false;
         }
-
-        econ.depositPlayer(player, rewardAmount);
-        int day = plugin.rewardManager.getConsecutiveDays(player);
-        player.sendMessage("あなたは" + day + "日目のログインボーナスを受け取りました。" + rewardAmount + "獲得しました。");
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String today = sdf.format(new Date());
-        playerData.set(player.getUniqueId().toString() + ".lastReceived", today); // 今日の日付を記録
-
-        // daysLoggedInを更新
-        daysLoggedIn = (daysLoggedIn >= 7) ? 1 : daysLoggedIn + 1; // 7日目を超えたらリセット
-        playerData.set(player.getUniqueId().toString() + ".daysLoggedIn", daysLoggedIn);
-        plugin.savePlayerDataConfig();
+        return true;
     }
 
-    public void open(Player player) {
-        player.openInventory(createGuiInventory(player));
-    }
+    private void giveReward (Player player) {
+            int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
 
-    private Inventory createGuiInventory(Player player) {
-        Inventory inv = Bukkit.createInventory(null, 9, "ログインボーナス"); // 9 slots titled "ログインボーナス"
-
-        for (int i = 0; i < 9; i++) {
-            if (i == 0 || i == 8) {
-                inv.setItem(i, createItem(Material.GRAY_STAINED_GLASS_PANE, "    ")); // 灰色のステンドグラス
-            } else if (i == 4) {
-                if (hasReceivedRewardToday(player)) {
-                    inv.setItem(i, createItem(Material.BARRIER, "報酬を受け取りました"));
-                } else {
-                    inv.setItem(i, createItem(Material.GOLD_INGOT, "ここをクリックしてログインボーナスを受け取る"));
-                }
-            } else {
-                inv.setItem(i, createItem(Material.IRON_INGOT, "まだ取得できません"));
+            double rewardAmount;
+            switch (daysLoggedIn) {
+                case 1:
+                    rewardAmount = 50;
+                    break;
+                case 2:
+                    rewardAmount = 100;
+                    break;
+                case 3:
+                    rewardAmount = 200;
+                    break;
+                case 4:
+                    rewardAmount = 400;
+                    break;
+                case 5:
+                    rewardAmount = 600;
+                    break;
+                case 6:
+                    rewardAmount = 800;
+                    break;
+                case 7:
+                    rewardAmount = 1000;
+                    break;
+                default:
+                    rewardAmount = 50;
+                    break;
             }
+
+            econ.depositPlayer(player, rewardAmount);
+            int day = plugin.rewardManager.getConsecutiveDays(player);
+            player.sendMessage("あなたは" + day + "日目のログインボーナスを受け取りました。" + rewardAmount + "獲得しました。");
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String today = sdf.format(new Date());
+            playerData.set(player.getUniqueId().toString() + ".lastReceived", today); // 今日の日付を記録
+
+            // daysLoggedInを更新
+            daysLoggedIn = (daysLoggedIn >= 7) ? 1 : daysLoggedIn + 1; // 7日目を超えたらリセット
+            playerData.set(player.getUniqueId().toString() + ".daysLoggedIn", daysLoggedIn);
+            plugin.savePlayerDataConfig();
         }
 
-        return inv;
-    }
+        public void open (Player player){
+            player.openInventory(createGuiInventory(player));
+        }
 
-    private ItemStack createItem(Material material, String displayName) {
-        ItemStack item = new ItemStack(material);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(displayName);
-        item.setItemMeta(meta);
-        return item;
-    }
+        private Inventory createGuiInventory (Player player){
+            Inventory inv = Bukkit.createInventory(null, 9, "ログインボーナス"); // 9 slots titled "ログインボーナス"
 
+            for (int i = 0; i < 9; i++) {
+                if (i == 0 || i == 8) {
+                    inv.setItem(i, createItem(Material.GRAY_STAINED_GLASS_PANE, "    ")); // 灰色のステンドグラス
+                } else if (i == 4) {
+                    if (hasReceivedRewardToday(player)) {
+                        inv.setItem(i, createItem(Material.BARRIER, "報酬を受け取りました"));
+                    } else {
+                        inv.setItem(i, createItem(Material.GOLD_INGOT, "ここをクリックしてログインボーナスを受け取る"));
+                    }
+                } else {
+                    inv.setItem(i, createItem(Material.IRON_INGOT, "まだ取得できません"));
+                }
+            }
+
+            return inv;
+        }
+
+        private ItemStack createItem (Material material, String displayName){
+            ItemStack item = new ItemStack(material);
+            ItemMeta meta = item.getItemMeta();
+            meta.setDisplayName(displayName);
+            item.setItemMeta(meta);
+            return item;
+        }
 }
+
