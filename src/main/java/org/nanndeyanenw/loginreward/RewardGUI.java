abda86a60c6c70af9aa196438189757b4e1a2c0f
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-18 19:09:43 +0900

    謎のエラー解消

--- src/main/java/org/nanndeyanenw/loginreward/RewardGUI.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardGUI.java
@@ -64,103 +64,103 @@ public class RewardGUI implements Listener {
     }
 
     private boolean hasReceivedRewardToday(Player player) {
-        String lastReceived = playerData.getString(player.getUniqueId().toString() + ".lastReceived", "");
+        String uniqueId = player.getUniqueId().toString();
+        if (!playerData.contains(uniqueId + ".lastReceived")) {
+            // 初回ログインの場合、データをセットせずにfalseを返す
+            return false;
+        }
+
+        String lastReceived = playerData.getString(uniqueId + ".lastReceived", "");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String today = sdf.format(new Date());
 
-        Bukkit.getLogger().info("Checking data for " + player.getName());
-        Bukkit.getLogger().info("Today: " + today);
-        Bukkit.getLogger().info("Last received: " + lastReceived);
-        Bukkit.getLogger().info("Today: " + today); //デバッグ用コード
-        Bukkit.getLogger().info("Last received: " + lastReceived); //デバッグ用コード
-
         if (!today.equals(lastReceived)) {
-            playerData.set(player.getUniqueId().toString() + ".lastReceived", today);
+            playerData.set(uniqueId + ".lastReceived", today);
             plugin.savePlayerDataConfig(); // ここでplayerDataを実際のファイルに保存
             return false;
         }
         return true;
-    }
-
-    private double giveReward(Player player) {
-        int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
-        double rewardAmount;
-        switch (daysLoggedIn) {
-            case 1:
-                rewardAmount = 50;
-                break;
-            case 2:
-                rewardAmount = 100;
-                break;
-            case 3:
-                rewardAmount = 200;
-                break;
-            case 4:
-                rewardAmount = 400;
-                break;
-            case 5:
-                rewardAmount = 600;
-                break;
-            case 6:
-                rewardAmount = 800;
-                break;
-            case 7:
-                rewardAmount = 1000;
-                break;
-            default:
-                rewardAmount = 50;
-                break;
         }
 
-        econ.depositPlayer(player, rewardAmount);
-        return rewardAmount; // 報酬の金額を返す
-    }
+        private double giveReward (Player player){
+            int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
+            double rewardAmount;
+            switch (daysLoggedIn) {
+                case 1:
+                    rewardAmount = 50;
+                    break;
+                case 2:
+                    rewardAmount = 100;
+                    break;
+                case 3:
+                    rewardAmount = 200;
+                    break;
+                case 4:
+                    rewardAmount = 400;
+                    break;
+                case 5:
+                    rewardAmount = 600;
+                    break;
+                case 6:
+                    rewardAmount = 800;
+                    break;
+                case 7:
+                    rewardAmount = 1000;
+                    break;
+                default:
+                    rewardAmount = 50;
+                    break;
+            }
+
+            econ.depositPlayer(player, rewardAmount);
+            return rewardAmount; // 報酬の金額を返す
+        }
 
 
-    private void handleRewardForPlayer(Player player) {
-        int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
-        double rewardAmount = giveReward(player);
+        private void handleRewardForPlayer (Player player){
+            int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1); // デフォルトは1日目
+            double rewardAmount = giveReward(player);
 
-        player.sendMessage("あなたは" + daysLoggedIn + "日目のログインボーナスを受け取りました。" + rewardAmount + "円を獲得しました！");
+            player.sendMessage("あなたは" + daysLoggedIn + "日目のログインボーナスを受け取りました。" + rewardAmount + "円を獲得しました！");
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String today = sdf.format(new Date());
-        playerData.set(player.getUniqueId().toString() + ".lastReceived", today); // 今日の日付を記録
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String today = sdf.format(new Date());
+            playerData.set(player.getUniqueId().toString() + ".lastReceived", today); // 今日の日付を記録
 
-        // daysLoggedInを更新
-        daysLoggedIn = (daysLoggedIn >= 7) ? 1 : daysLoggedIn + 1; // 7日目を超えたらリセット
-        playerData.set(player.getUniqueId().toString() + ".daysLoggedIn", daysLoggedIn);
+            // daysLoggedInを更新
+            daysLoggedIn = (daysLoggedIn >= 7) ? 1 : daysLoggedIn + 1; // 7日目を超えたらリセット
+            playerData.set(player.getUniqueId().toString() + ".daysLoggedIn", daysLoggedIn);
 
-        plugin.savePlayerDataConfig();
-    }
+            plugin.savePlayerDataConfig();
+        }
 
 
-    public void open(Player player) {
-        player.openInventory(createGuiInventory(player));
-    }
+        public void open (Player player){
+            player.openInventory(createGuiInventory(player));
+        }
 
-    private Inventory createGuiInventory(Player player) {
-        Inventory inv = Bukkit.createInventory(null, 9, "ログインボーナス");
-        int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1);
-        Bukkit.getLogger().info("Player " + player.getName() + " has logged in for " + daysLoggedIn + " days.");
+        private Inventory createGuiInventory (Player player){
+            Inventory inv = Bukkit.createInventory(null, 9, "ログインボーナス");
+            int daysLoggedIn = playerData.getInt(player.getUniqueId().toString() + ".daysLoggedIn", 1);
+            Bukkit.getLogger().info("Player " + player.getName() + " has logged in for " + daysLoggedIn + " days.");
 
-        for (int i = 0; i < 9; i++) {
-            if (i == 0 || i == 8) {
-                inv.setItem(i, createItem(Material.GRAY_STAINED_GLASS_PANE, "    ")); // 灰色のステンドグラス
-            } else if (i == daysLoggedIn) { // 今日受け取るべき日の報酬
-                if (hasReceivedRewardToday(player)) {
+            for (int i = 0; i < 9; i++) {
+                if (i == 0 || i == 8) {
+                    inv.setItem(i, createItem(Material.GRAY_STAINED_GLASS_PANE, "    ")); // 灰色のステンドグラス
+                } else if (i == daysLoggedIn) { // 今日受け取るべき日の報酬
+                    if (hasReceivedRewardToday(player)) {
+                        inv.setItem(i, createItem(Material.BARRIER, "報酬を受け取りました"));
+                    } else {
+                        inv.setItem(i, createItem(Material.GOLD_INGOT, "ここをクリックしてログインボーナスを受け取る"));
+                    }
+                } else if (i < daysLoggedIn) { // 既に受け取った日の報酬
                     inv.setItem(i, createItem(Material.BARRIER, "報酬を受け取りました"));
                 } else {
-                    inv.setItem(i, createItem(Material.GOLD_INGOT, "ここをクリックしてログインボーナスを受け取る"));
+                    inv.setItem(i, createItem(Material.IRON_INGOT, "まだ取得できません"));
                 }
-            } else if (i < daysLoggedIn) { // 既に受け取った日の報酬
-                inv.setItem(i, createItem(Material.BARRIER, "報酬を受け取りました"));
-            } else {
-                inv.setItem(i, createItem(Material.IRON_INGOT, "まだ取得できません"));
             }
+            return inv;
         }
-        return inv;
-    }
 
 
         private ItemStack createItem (Material material, String displayName){
@@ -171,3 +171,4 @@ public class RewardGUI implements Listener {
             return item;
         }
 }
+
