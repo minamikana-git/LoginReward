e29482384865f66d6ad6a888129b0fa3742c854c
minamikana-git <yuiwiz928_sub@yahoo.co.jp>
2023-10-27 14:54:30 +0900

    [fix] RewardManager.java

--- src/main/java/org/nanndeyanenw/loginreward/RewardManager.java
+++ src/main/java/org/nanndeyanenw/loginreward/RewardManager.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +16,7 @@ import java.util.UUID;
 
 public class RewardManager {
 
-    private DataUtil dataUtil;
+
     private Map<UUID, Double> playerMoney = new HashMap<>();
 
     private Reward loginReward = new Reward(100, "ログインボーナス!");
@@ -73,13 +74,15 @@ public class RewardManager {
         }
 
     public Reward getRewardForConsecutiveDays(int days) {
-        if (days >= 7) {
-            return rewards.get(7);
-        }
-        return rewards.getOrDefault(days, null);
+
+        return getRewardForDay(days);
     }
 
     public void giveLoginReward(Player player) {
+
+        if (isConsecutiveLoginBroken(player)) {
+            setConsecutiveDays(player, 1);
+        }
 
         int consecutiveDays = getConsecutiveDays(player);
         Reward reward = getRewardForConsecutiveDays(consecutiveDays);
@@ -91,9 +94,13 @@ public class RewardManager {
             // 連続ログイン日数を1増やす (もし7日を超えたら1日目にリセット)
             setConsecutiveDays(player, (consecutiveDays % 7) + 1);
         }
+
+        // ログイン日を更新
+        setLastLoginDate(player, LocalDate.now());
     }
 
-        public void setConsecutiveDays(Player player, int days) {
+
+    public void setConsecutiveDays(Player player, int days) {
             dataConfig.set(player.getUniqueId() + ".consecutiveDays", days);
             saveYAML();
         }
@@ -124,17 +131,27 @@ public class RewardManager {
 
 
     public Reward getRewardForDay(int day) {
-        return switch (day) {
-            case 1 -> new Reward(50, "ログインボーナス！: 50NANDE!");
-            case 2 -> new Reward(100, "2連続ログインボーナス！: 100NANDE!");
-            case 3 -> new Reward(200, "3連続ログインボーナス！: 200NANDE!");
-            case 4 -> new Reward(400, "4連続ログインボーナス！: 400NANDE!");
-            case 5 -> new Reward(600, "5連続ログインボーナス！: 600NANDE!");
-            case 6 -> new Reward(800, "6連続ログインボーナス！: 800NANDE!");
-            case 7 -> new Reward(1000, "7連続ログインボーナス！: 1000NANDE!");
-            default -> null;
-        };
+        switch (day) {
+            case 1:
+                return new Reward(50, "ログインボーナス！: 50NANDE!");
+            case 2:
+                return new Reward(100, "2連続ログインボーナス！: 100NANDE!");
+            case 3:
+                return new Reward(200, "3連続ログインボーナス！: 200NANDE!");
+            case 4:
+                return new Reward(400, "4連続ログインボーナス！: 400NANDE!");
+            case 5:
+                return new Reward(600, "5連続ログインボーナス！: 600NANDE!");
+            case 6:
+                return new Reward(800, "6連続ログインボーナス！: 800NANDE!");
+            case 7:
+                return new Reward(1000, "7連続ログインボーナス！: 1000NANDE!");
+            default:
+                return new Reward(1000, "7連続ログインボーナス以上！: 1000NANDE!");
+        }
     }
+
+
 
     public boolean hasClaimedReward(Player player) {
         // 現在の連続ログイン日数を取得
@@ -174,6 +191,24 @@ public class RewardManager {
         // ファイルに保存
         saveYAML();
     }
+    public void setLastLoginDate(Player player, LocalDate date) {
+        dataConfig.set(player.getUniqueId() + ".lastLoginDate", date.toString());
+    }
+    public LocalDate getLastLoginDate(Player player) {
+        String dateString = dataConfig.getString(player.getUniqueId() + ".lastLoginDate", null);
+        return dateString != null ? LocalDate.parse(dateString) : null;
+    }
+    public boolean isConsecutiveLoginBroken(Player player) {
+        LocalDate lastLoginDate = getLastLoginDate(player);
+        if (lastLoginDate == null) {
+            return true; // 初めてのログインとして扱います
+        }
+        return !lastLoginDate.plusDays(1).equals(LocalDate.now());
+    }
+
+
+
+
 
 
 }
